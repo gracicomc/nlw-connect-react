3bc580f3ff1baa9c18b1178b27b40d611bc44b0c
Graci <graciparentte@gmail.com>
2025-02-17 23:45:40 -0300

    init project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,29 @@
+import { Button } from "@/components/Button/Button";
+import { IconButton } from "@/components/IconButton/IconButton";
+import { InputField, InputIcon, InputRoot } from "@/components/Input/Input";
+import { ArrowRight, Mail } from "lucide-react";
+
 export default function Home() {
   return (
     <main>
-      <div>Hello world!</div>
+      <Button>
+        teste
+        <ArrowRight />
+      </Button>
+
+      <IconButton>
+        <ArrowRight />
+      </IconButton>
+
+      <div>
+        <InputRoot>
+          <InputIcon>
+            <Mail />
+          </InputIcon>
+
+          <InputField />
+        </InputRoot>
+      </div>
     </main>
   );
 }
