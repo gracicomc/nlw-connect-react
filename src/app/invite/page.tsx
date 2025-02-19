4d63f945c7f6ec10c2ca4550873baa91ccbae7ca
Graci <graciparentte@gmail.com>
2025-02-19 20:52:08 -0300

    feat: finish layouts

--- src/app/invite/page.tsx
+++ src/app/invite/page.tsx
@@ -1,3 +1,136 @@
+import Image from "next/image";
+import { InputField, InputIcon, InputRoot } from "@/components/Input/Input";
+import { BadgeCheck, Copy, Link, Medal, MousePointerClick } from "lucide-react";
+import { IconButton } from "@/components/IconButton/IconButton";
+
+import logo from "../../assets/logo.svg";
+import gold from "../../assets/medal-gold.svg";
+import silver from "../../assets/medal-silver.svg";
+import cooper from "../../assets/medal-cooper.svg";
+
 export default function InvitePage() {
-  return <h1>oi</h1>;
+  return (
+    <div className="min-h-dvh flex items-center justify-between gap-16 flex-col md:flex-row">
+      <div className="flex flex-col gap-10 w-full max-w-[550px]">
+        <Image src={logo} alt="devstage" className="h-[30px] w-[108.5px]" />
+        <div className="space-y-2">
+          <h1 className="text-4xl font-semibold font-heading text-gray-100 leading-none">
+            Inscrição confirmada!
+          </h1>
+          <p className="text-gray-300">
+            Para entrar no evento, acesse o link enviado para seu e-mail.
+          </p>
+        </div>
+
+        <div className="space-y-6">
+          <div className="space-y-3">
+            <h2 className="text-gray-200 text-xl font-heading font-semibold leading-none">
+              Indique e ganhe
+            </h2>
+            <p className="text-gray-200">
+              Convide mais pessoas para o evento e concorra a prêmios
+              exclusivos! É só compartilhar o link abaixo e acompanhar as
+              inscrições:
+            </p>
+          </div>
+
+          <InputRoot>
+            <InputIcon>
+              <Link className="size-5" />
+            </InputIcon>
+            <InputField
+              readOnly
+              defaultValue={"http://localhost:3000/invite"}
+            />
+
+            <IconButton className="-mr-2">
+              <Copy className="size-5" />
+            </IconButton>
+          </InputRoot>
+
+          <div className="grid gap-3 md:grid-cols-3">
+            <div className="relative bg-gray-700 border border-gray-600 px-4 py-7 flex flex-col items-center justify-center gap-1 rounded-xl">
+              <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
+                1042
+              </span>
+              <span className="text-sm text-gray-300 leading-none text-center">
+                acessos ao link
+              </span>
+              <MousePointerClick className="size-5 text-purple absolute top-3 left-3" />
+            </div>
+
+            <div className="relative bg-gray-700 border border-gray-600 px-4 py-7 flex flex-col items-center justify-center gap-1 rounded-xl">
+              <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
+                1042
+              </span>
+              <span className="text-sm text-gray-300 leading-none text-center">
+                Inscrições feitas
+              </span>
+              <BadgeCheck className="size-5 text-purple absolute top-3 left-3" />
+            </div>
+
+            <div className="relative bg-gray-700 border border-gray-600 px-4 py-7 flex flex-col items-center justify-center gap-1 rounded-xl">
+              <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
+                3
+              </span>
+              <span className="text-sm text-gray-300 leading-none text-center">
+                Posição no ranking
+              </span>
+              <Medal className="size-5 text-purple absolute top-3 left-3" />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="w-full max-w-[440px] space-y-5">
+        <h2 className="text-gray-200 text-xl font-heading font-semibold leading-none">
+          Ranking de indicações
+        </h2>
+
+        <div className="space-y-4">
+          <div className="relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3">
+            <span className="text-sm text-gray-300 leading-none">
+              <span className="font-semibold">1</span> | Diego Fernandes
+            </span>
+
+            <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
+              1030
+            </span>
+            <Image
+              src={gold}
+              alt="gold medal"
+              className="absolute top-0 right-8"
+            />
+          </div>
+          <div className="relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3">
+            <span className="text-sm text-gray-300 leading-none">
+              {" "}
+              <span className="font-semibold">1</span> | Diego Fernandes
+            </span>
+            <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
+              1030
+            </span>
+            <Image
+              src={silver}
+              alt="silver medal"
+              className="absolute top-0 right-8"
+            />
+          </div>
+          <div className="relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3">
+            <span className="text-sm text-gray-300 leading-none">
+              <span className="font-semibold">1</span> | Diego Fernandes
+            </span>
+            <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
+              1030
+            </span>
+            <Image
+              src={cooper}
+              alt="cooper medal"
+              className="absolute top-0 right-8"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/components/IconButton/IconButton.tsx
+++ src/components/IconButton/IconButton.tsx
@@ -1,12 +1,19 @@
-import { ComponentProps, ReactNode } from "react";
+import { ComponentProps } from "react";
+import { twMerge } from "tailwind-merge";
 
 interface IconButtonProps extends ComponentProps<"button"> {}
 
-export function IconButton(props: IconButtonProps) {
+export function IconButton({ className, ...props }: IconButtonProps) {
   return (
     <button
-      className="p-1.5 bg-gray-500 text-blue  rounded-md  cursor-pointer transition-colors duration-300 hover:bg-blue hover:text-gray-900"
+      className={twMerge(
+        "p-1.5 bg-gray-500 text-blue  rounded-md  cursor-pointer transition-colors duration-300 hover:bg-blue hover:text-gray-900",
+        className
+      )}
       {...props}
     />
   );
 }
+
+//com a lib tailwind merge, é possível seguir estilizando com atributos className em outros elementos, fazendo assim a união do que foi passado no className onde o componente é chamado, impedindo que uma classe sobrescreva outra. Teria a possibildide de apenas passar como string, mas no caso de atriburos duplicados, como que o tailwind saberá qual usar? o Twmerge soluciona esse problema ao identificar qual classe está sendo modificada e substitui pela nova, não deixando existir classes duplicadas
+//o uso é extrair o className das props e passar como elemento javascript dentro do className, chamando a função twMerge()
